86499d7630be2eb6e127b8458370634aa18a5ac7
German <german_gonzalez@hotmail.com>
2018-09-07 08:14:38 -0300

    Cambios Locales

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 
-"""
+
 
 from django.contrib import admin
 from django.urls import path
@@ -24,11 +24,21 @@ urlpatterns = [
 
 """
 
+from django.urls import include, path
+from django.contrib import admin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path(r'', include('blog.urls')),
+]
+
+"""
+
 from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls')),
+    url(r'', include  ('blog.urls')),
 ]
 """
